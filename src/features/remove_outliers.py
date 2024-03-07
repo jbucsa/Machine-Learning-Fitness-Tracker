13563dcceaa005268585e100ba8a410cbeff79ec
jbucsa <justin.bucsa@gmail.com>
2024-03-07 14:44:03 -0600

    Create template file for src/features/remove_outliers.py

---
+++ src/features/remove_outliers.py
@@ -0,0 +1,70 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+import scipy
+from sklearn.neighbors import LocalOutlierFactor  # pip install scikit-learn
+
+# --------------------------------------------------------------
+# Load data
+# --------------------------------------------------------------
+
+
+# --------------------------------------------------------------
+# Plotting outliers
+# --------------------------------------------------------------
+
+
+# --------------------------------------------------------------
+# Interquartile range (distribution based)
+# --------------------------------------------------------------
+
+# Insert IQR function
+
+
+# Plot a single column
+
+
+# Loop over all columns
+
+
+# --------------------------------------------------------------
+# Chauvenets criteron (distribution based)
+# --------------------------------------------------------------
+
+# Check for normal distribution
+
+
+# Insert Chauvenet's function
+
+
+# Loop over all columns
+
+
+# --------------------------------------------------------------
+# Local outlier factor (distance based)
+# --------------------------------------------------------------
+
+# Insert LOF function
+
+
+# Loop over all columns
+
+
+# --------------------------------------------------------------
+# Check outliers grouped by label
+# --------------------------------------------------------------
+
+
+# --------------------------------------------------------------
+# Choose method and deal with outliers
+# --------------------------------------------------------------
+
+# Test on single column
+
+
+# Create a loop
+
+# --------------------------------------------------------------
+# Export new dataframe
+# --------------------------------------------------------------
