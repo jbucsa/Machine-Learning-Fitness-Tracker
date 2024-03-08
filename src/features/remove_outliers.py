4c4d98dae64181ba2dbab2736d47905bbc0f3ed1
jbucsa <justin.bucsa@gmail.com>
2024-03-08 13:58:19 -0600

    Added plot_binary_outliers function to help remove outliers.

--- src/features/remove_outliers.py
+++ src/features/remove_outliers.py
@@ -43,6 +43,51 @@ df[outlier_columns[0:3] + ["label"]].boxplot(by="label", figsize=(20,10), layout
 
 df[outlier_columns[3:6] + ["label"]].boxplot(by="label", figsize=(20,10), layout=(1, 3))
 
+def plot_binary_outliers(dataset, col, outlier_col, reset_index):
+    """ Plot outliers in case of a binary outlier score. Here, the col specifies the real data
+    column and outlier_col the columns with a binary value (outlier or not).
+
+    Args:
+        dataset (pd.DataFrame): The dataset
+        col (string): Column that you want to plot
+        outlier_col (string): Outlier column marked with true/false
+        reset_index (bool): whether to reset the index for plotting
+    """
+
+    # Taken from: https://github.com/mhoogen/ML4QS/blob/master/Python3Code/util/VisualizeDataset.py
+
+    dataset = dataset.dropna(axis=0, subset=[col, outlier_col])
+    dataset[outlier_col] = dataset[outlier_col].astype("bool")
+
+    if reset_index:
+        dataset = dataset.reset_index()
+
+    fig, ax = plt.subplots()
+
+    plt.xlabel("samples")
+    plt.ylabel("value")
+
+    # Plot non outliers in default color
+    ax.plot(
+        dataset.index[~dataset[outlier_col]],
+        dataset[col][~dataset[outlier_col]],
+        "+",
+    )
+    # Plot data points that are outliers in red
+    ax.plot(
+        dataset.index[dataset[outlier_col]],
+        dataset[col][dataset[outlier_col]],
+        "r+",
+    )
+
+    plt.legend(
+        ["outlier " + col, "no outlier " + col],
+        loc="upper center",
+        ncol=2,
+        fancybox=True,
+        shadow=True,
+    )
+    plt.show()
 
 # --------------------------------------------------------------
 # Interquartile range (distribution based)
