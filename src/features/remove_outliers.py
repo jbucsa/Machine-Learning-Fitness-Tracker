493362327ba4051b77e10788c47392cf445c711f
jbucsa <justin.bucsa@gmail.com>
2024-03-08 14:17:55 -0600

    Visualizing outliers.

--- src/features/remove_outliers.py
+++ src/features/remove_outliers.py
@@ -89,14 +89,49 @@ def plot_binary_outliers(dataset, col, outlier_col, reset_index):
     )
     plt.show()
 
+
 # --------------------------------------------------------------
 # Interquartile range (distribution based)
 # --------------------------------------------------------------
 
 # Insert IQR function
 
+def mark_outliers_iqr(dataset, col):
+    """Function to mark values as outliers using the IQR method.
+
+    Args:
+        dataset (pd.DataFrame): The dataset
+        col (string): The column you want apply outlier detection to
+
+    Returns:
+        pd.DataFrame: The original dataframe with an extra boolean column 
+        indicating whether the value is an outlier or not.
+    """
+
+    dataset = dataset.copy()
+
+    Q1 = dataset[col].quantile(0.25)
+    Q3 = dataset[col].quantile(0.75)
+    IQR = Q3 - Q1
+
+    lower_bound = Q1 - 1.5 * IQR
+    upper_bound = Q3 + 1.5 * IQR
+
+    dataset[col + "_outlier"] = (dataset[col] < lower_bound) | (
+        dataset[col] > upper_bound
+    )
+
+    return dataset
+
+
+
 
 # Plot a single column
+
+col = "acc_x"
+dataset = mark_outliers_iqr(df, col)
+
+plot_binary_outliers(dataset=dataset, col=col, outlier_col=col+"_outlier", reset_index=True)
 
 
 # Loop over all columns
