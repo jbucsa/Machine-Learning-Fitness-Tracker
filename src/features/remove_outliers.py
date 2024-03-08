4bd698ce147787a07c1f8fa88af7eae3e823dfd5
jbucsa <justin.bucsa@gmail.com>
2024-03-08 13:56:25 -0600

    Finding outliers

--- src/features/remove_outliers.py
+++ src/features/remove_outliers.py
@@ -11,6 +11,16 @@ from sklearn.neighbors import LocalOutlierFactor  # pip install scikit-learn
 
 df = pd.read_pickle("../../data/interim/01_data_processed.pkl")
 
+""" 
+This variable just calls out the first 6 columns in the data, these are the columns that hold our x, y, and z data. THEN we convert the INDEX to a LIST.
+
+outlier_columns = df.columns[0:6] >> Index(['acc_x', 'acc_y', 'acc_z', 'gyr_x', 'gyr_y', 'gyr_z'], dtype='object')
+
+outlier_columns = list(df.columns[0:6]) >> ['acc_x', 'acc_y', 'acc_z', 'gyr_x', 'gyr_y', 'gyr_z'] 
+"""
+
+outlier_columns = list(df.columns[0:6])
+
 # --------------------------------------------------------------
 # Plotting outliers
 # --------------------------------------------------------------
@@ -19,7 +29,20 @@ plt.style.use("fivethirtyeight")
 plt.rcParams["figure.figsize"] = (20, 5)
 plt.rcParams["figure.dpi"]= 100
 
-df[["acc_x"]].boxplot()
+# First step is to get a visualization of what is seen
+df[["acc_x", "label"]].boxplot(by="label", figsize=(20,10))
+df[["acc_y", "label"]].boxplot(by="label", figsize=(20,10))
+df[["acc_z", "label"]].boxplot(by="label", figsize=(20,10))
+
+df[["gyr_x", "label"]].boxplot(by="label", figsize=(20,10))
+df[["gyr_y", "label"]].boxplot(by="label", figsize=(20,10))
+df[["gyr_z", "label"]].boxplot(by="label", figsize=(20,10))
+
+
+df[outlier_columns[0:3] + ["label"]].boxplot(by="label", figsize=(20,10), layout=(1, 3))
+
+df[outlier_columns[3:6] + ["label"]].boxplot(by="label", figsize=(20,10), layout=(1, 3))
+
 
 # --------------------------------------------------------------
 # Interquartile range (distribution based)
