97752fe299492584a93b82585f970627e3427f09
jbucsa <justin.bucsa@gmail.com>
2024-03-13 19:23:22 -0500

    Update src/features/build_features.py file. INTERPOLATE started.

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -9,10 +9,34 @@ from TemporalAbstraction import NumericalAbstraction
 # Load data
 # --------------------------------------------------------------
 
+df = pd.read_pickle("../../data/interim/02_outliers_removed_chauvenets.pkl")
+
+predictor_columns = list(df.columns[0:6])
+
+
+#  Plot Settings
+plt.style.use("fivethirtyeight")
+plt.rcParams["figure.figsize"] = (20, 5)
+plt.rcParams["figure.dpi"]= 100
+plt.rcParams["lines.linewidth"] = 2
+
+
+# Example of a plot for a subset
+
+df.info()
+
+subset = df[df["set"] == 35]["gyr_y"].plot()
+
 
 # --------------------------------------------------------------
 # Dealing with missing values (imputation)
 # --------------------------------------------------------------
+
+# INTERPOLATE will fill in the gap were the data is missing.
+for col in predictor_columns:
+    df[col] = df[col].interpolate()
+    
+df.info()
 
 
 # --------------------------------------------------------------
