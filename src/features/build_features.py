e6ce9c802ab6ecb37165cbc6f0de3db95ffcc4b1
jbucsa <justin.bucsa@gmail.com>
2024-03-18 14:46:33 -0500

    Prepping data for filters.

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -11,20 +11,18 @@ from TemporalAbstraction import NumericalAbstraction
 
 df = pd.read_pickle("../../data/interim/02_outliers_removed_chauvenets.pkl")
 
-predictor_columns = list(df.columns[0:6])
+predictor_columns = list(df.columns[:6])
 
 
 #  Plot Settings
 plt.style.use("fivethirtyeight")
 plt.rcParams["figure.figsize"] = (20, 5)
-plt.rcParams["figure.dpi"]= 100
+plt.rcParams["figure.dpi"] = 100
 plt.rcParams["lines.linewidth"] = 2
 
 
 # Example of a plot for a subset
-
 df.info()
-
 subset = df[df["set"] == 35]["gyr_y"].plot()
 
 
@@ -43,6 +41,27 @@ df.info()
 # Calculating set duration
 # --------------------------------------------------------------
 
+df[df["set"] == 25]["acc_y"].plot()
+df[df["set"] == 50]["acc_y"].plot()
+
+#  Time of the 'LAST-Index' minus Time of the 'FIRST-Index'
+duration = df[df["set"] == 1].index[-1] - df[df["set"] == 1].index[0] 
+duration.seconds
+
+#  LOOP to find the average DURATION among the unique sets
+for s in df["set"].unique():
+
+    start = df[df["set"] == s].index[0]
+    stop = df[df["set"] == s].index[-1]
+    
+    duration =  stop - start
+    df.loc[(df["set"] == s), "duration"] = duration.seconds
+
+duration_df = df.groupby(["category"])["duration"].mean()
+
+duration_df.iloc[0] / 5
+
+duration_df.iloc[1] / 10
 
 # --------------------------------------------------------------
 # Butterworth lowpass filter
