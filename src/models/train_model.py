8de302fc34b21d5bb2f96a855af3c2d402204019
jbucsa <justin.bucsa@gmail.com>
2024-04-06 16:56:46 -0500

    Created Confusion Matrix

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -198,17 +198,62 @@ for i, f in zip(range(len(possible_feature_sets)), feature_names):
 # Create a grouped bar plot to compare the results
 # --------------------------------------------------------------
 
-score_df
+score_df.sort_values(by="accuracy", ascending=False,)
 
+# Below plot will create a bar chart to compare the accuracy of the models
+plt.figure(figure=(10, 10))
+sns.barplot(x="model", y="accuracy", hue="feature_set", data=score_df)
+plt.xlabel("Model")
+plt.ylabel("Accuracy")
+plt.ylim(0.7, 1)
+plt.legend(loc="lower right")
+plt.show()
 
 # --------------------------------------------------------------
 # Select best model and evaluate results
 # --------------------------------------------------------------
 
 
+# Code below comes from the Training random forest feature within the import code
+(class_train_y, class_test_y, class_train_prob_y, class_test_prob_y, ) = learner.random_forest( X_train[feature_set_4], y_train, X_test[feature_set_4], gridsearch=True )
+
+accuracy =  accuracy_score( y_test, class_test_y)
+
+classes = class_test_prob_y.columns
+# Define confusion matrix
+cm = confusion_matrix(y_test, class_test_y, labels=classes)
+
+# Copy and Paste Confusion Matrix Code
+# create confusion matrix for cm
+plt.figure(figsize=(10, 10))
+plt.imshow(cm, interpolation="nearest", cmap=plt.cm.Blues)
+plt.title("Confusion matrix")
+plt.colorbar()
+tick_marks = np.arange(len(classes))
+plt.xticks(tick_marks, classes, rotation=45)
+plt.yticks(tick_marks, classes)
+
+thresh = cm.max() / 2.0
+for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+    plt.text(
+        j,
+        i,
+        format(cm[i, j]),
+        horizontalalignment="center",
+        color="white" if cm[i, j] > thresh else "black",
+    )
+plt.ylabel("True label")
+plt.xlabel("Predicted label")
+plt.grid(False)
+plt.show()
+
+
+
+
 # --------------------------------------------------------------
 # Select train and test data based on participant
 # --------------------------------------------------------------
+
 
 
 # --------------------------------------------------------------
