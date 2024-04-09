836b1693b5c7ace60d41e4b146ca49a87b7717af
jbucsa <justin.bucsa@gmail.com>
2024-04-09 14:15:32 -0500

    Update to Create benchmark dataframe

--- src/features/count_repetitions.py
+++ src/features/count_repetitions.py
@@ -89,11 +89,71 @@ LowPass.low_pass_filter(
 # Create function to count repetitions
 # --------------------------------------------------------------
 
+column = "acc_r"
+data = LowPass.low_pass_filter(
+    bench_set, col=column, sampling_frequency=fs, cutoff_frequency=0.4, order=10
+)
+
+indexes = argrelextrema(data[column + "_lowpass"].values, np.greater)
+peaks = data.iloc[indexes]
+
+def count_reps(dataset, cutoff=0.4, order=10, column="acc_r"):
+    data = LowPass.low_pass_filter(
+        dataset, col=column, sampling_frequency=fs, cutoff_frequency=cutoff, order=order
+        )
+    indexes = argrelextrema(data[column + "_lowpass"].values, np.greater)
+    peaks = data.iloc[indexes]
+    
+    fig, ax = plt.subplots()
+    plt.plot(dataset[f"{column}_lowpass"])
+    plt.plot(peaks[f"{column}_lowpass"], "o", color="red")
+    ax.set_ylabel(f"{column}_lowpass")
+    exercise = dataset["label"].iloc[0].title()
+    category = dataset["category"].iloc[0].title()
+    plt.title(f"{category} {exercise}: {len(peaks)} Reps")
+    plt.show()
+
+    return len(peaks)
+
+count_reps(bench_set, cutoff=0.4)
+count_reps(squat_set, cutoff=0.3505)
+count_reps(row_set, cutoff=0.65, column="gyr_y")
+count_reps(dead_set, cutoff=0.385)
+count_reps(ohp_set, cutoff=0.35)
+
 
 # --------------------------------------------------------------
 # Create benchmark dataframe
 # --------------------------------------------------------------
 
+df["reps"] = df["category"].apply(lambda x:5 if x=="heavy" else 10)
+rep_df = df.groupby(["label", "category", "set"])["reps"].max().reset_index()
+rep_df["reps_pred"] = 0
+
+for s in df["set"].unique():
+    subset =  df[df["set"] ==s]
+    
+    column = "acc_r"
+    cutoff = 0.4
+    
+    if subset["label"].iloc[0] == "squat":
+        cutoff = 0.3505
+    
+    if subset["label"].iloc[0] == "row":
+        cutoff = 0.65
+        column = "gyr_y"
+    
+    if subset["label"].iloc[0] == "dead":
+        cutoff = 0.385
+    
+    if subset["label"].iloc[0] == "row":
+        cutoff = 0.35
+        
+    reps = count_reps(subset, cutoff=cutoff, column=column)
+    
+    rep_df.loc[rep_df["set"] == s, "reps_pred"] = reps
+
+rep_df
 
 # --------------------------------------------------------------
 # Evaluate the results
